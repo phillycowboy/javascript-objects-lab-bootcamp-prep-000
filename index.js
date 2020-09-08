38f4817c580c7030ae875c2b7aac0b366f177f3f
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-08 20:46:09 +0000

    Automatically backed up by Learn

---
+++ index.js
@@ -0,0 +1,20 @@
+var recipes =Object({eggs:3});
+
+function updateObjectWithKeyAndValue(object, key, value){
+  
+  return Object.assign({}, object, {[key]:value});
+  }
+  
+function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
+    object[key]=value;
+    return object;
+  }
+  
+function deleteFromObjectByKey(object, key){
+  
+  delete object[key];
+  return object;
+}
+  
+
+   
