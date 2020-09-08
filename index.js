a88ab681502493bbf9e3c883032b59a9be9dc267
Ben Harter-Murphy <benhartermurphy@gmail.com>
2020-09-08 20:48:08 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,8 +12,7 @@ function destructivelyUpdateObjectWithKeyAndValue(object, key, value){
   
 function deleteFromObjectByKey(object, key){
   
-  delete object["key"];
-  return object;
+  return Object.assign({})
 }
   
 
